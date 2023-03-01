a2972729a278c921d228518ad85c034ba61cd5dd
Pepa Mráz <pepamraz1@gmail.com>
2023-03-01 22:20:19 +0100

    Úkol 3

--- 3.sh
+++ 3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+z=$IFS
+IFS=$'\n'
+
+nejvetsi_cas=0
+nazev_souboru=""
+
+for file in $(ls -1 ~/Plocha)
+    do
+
+    cas=$(stat --format=%Z ~/Plocha/$file)
+    if [ $cas -gt $nejvetsi_cas ]
+        then
+        nejvetsi_cas=$cas
+        nazev_souboru=$file
+        fi
+
+    done
+
+echo "Naposledy upravený soubor/složka na Ploše je: "
+echo "$nazev_souboru"
+
+IFS=$z
