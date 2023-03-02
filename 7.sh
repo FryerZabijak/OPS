670849b1581ae44cc2ba1ecf21b95e41b3f3fc99
Pepa Mráz <pepamraz1@gmail.com>
2023-03-02 19:27:04 +0100

    Hotový program č.7

---
+++ 7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Skript vypíše loginy všech uživatelů, jejichž domovský adresář neexistuje"
+
+z=$IFS
+IFS=$'\n'
+
+file_passwd=$(cat /etc/passwd)
+
+for line in $file_passwd
+do
+path=$(echo $line | cut -d":" -f6)
+if [ ! -d $path ]
+then
+    login=$(echo $line | cut -d":" -f1)
+    echo $login
+fi
+done
+
+IFS=$z
